fcb007f241c17eadc7ec425aa4b540556f8a63df
patrickjoh <patrick.johannessen@hotmail.com>
2023-04-19 11:08:15 +0200

    Initial working build of website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
+import About from './About';
+import Projects from './Projects';
+import './css/App.css';
+import ThemeContext from './ThemeContext';
 
 function App() {
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+
+  useEffect(() => {
+    document.body.classList.toggle('dark', darkMode);
+    document.body.classList.toggle('light', !darkMode);
+  }, [darkMode]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={`App ${darkMode ? 'dark' : ''}`}>
+        <header>
+          <div className="name-container">
+            <h1 className="name">Patrick<br />Johannessen</h1>
+          </div>
+          <nav>
+            <ul className="nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to="/about">About Me</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" activeClassName="active" to="/projects">Projects</NavLink>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://github.com/patrickjoh" target="_blank" rel="noopener noreferrer">GitHub</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.linkedin.com/in/patrick-johannessen/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+              </li>
+              <li>
+                <button className="toggle-theme" onClick={toggleDarkMode}>Toggle Theme</button>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="main-content">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
